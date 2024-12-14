cfbb20ad06541af4b15a0aa8989e5ee6fca17260
jrodriguez2790 <157295161+jrodriguez2790@users.noreply.github.com>
2024-12-14 14:24:41 -0500

    adds citysearch test
    If applied, this commit will have added the citysearch.test.js file for local testing.

--- src/_tests_/CitySearch.test.js
+++ src/_tests_/CitySearch.test.js
@@ -1,1 +1,41 @@
+// src/__tests__/CitySearch.test.js
+import React from 'react';
+import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import CitySearch from './CitySearch';
+import { extractLocations, getEvents } from '../api';
 
+describe('<CitySearch /> component', () => {
+  let CitySearchComponent;
+  beforeEach(() => {
+    CitySearchComponent = render(<CitySearch />);
+  });
+
+  test('renders text input', async () => {
+    const user = userEvent.setup();
+    const cityTextBox = CitySearchComponent.queryByRole('textbox');
+    await user.click(cityTextBox);
+    const suggestionList = CitySearchComponent.queryByRole('list');
+    expect(suggestionList).toBeInTheDocument();
+    expect(suggestionList).toHaveClass('suggestions');
+  });
+
+  test('updates list of suggestions correctly when user types in city textbox', async () => {
+    const user = userEvent.setup();
+    const allEvents = await getEvents();
+    const allLocations = extractLocations(allEvents);
+    CitySearchComponent.rerender(<CitySearch allLocations={allLocations} />);
+
+    const cityTextBox = CitySearchComponent.queryByRole('textbox');
+    await user.type(cityTextBox, 'Berlin');
+
+    const filteredLocations = allLocations.filter(location =>
+      location.toUpperCase().includes('Berlin'.toUpperCase())
+    );
+
+    const suggestionListItems = CitySearchComponent.queryAllByRole('listitem');
+    expect(suggestionListItems).toHaveLength(filteredLocations.length + 1);
+  });
+});
+
+export default CitySearch;
